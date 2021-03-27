a5727cee8a4f0acc3f4e5b76c3ee5b44e887d3fb
Matheus Azevedo <matheus.azevedo@qdois.com.br>
2021-03-27 09:53:08 -0300

    Correcting vars.py

--- vars.py
+++ vars.py
@@ -1,38 +1,30 @@
 class User:
     active_users = []
 
-
-    def __init__ (self, name, email):
+    def __init__(self, name, email):
         self.name = name
         self.email = email
-
 
     def activate(self):
         if self not in self.__class__.active_users:
             self.__class__.active_users.append(self)
 
     def deactivate(self):
-        if self.is_active():
+        if self in self.__class__.active_users:
             self.__class__.active_users.remove(self)
-    
 
     def is_active(self):
         return self in self.__class__.active_users
 
-me = User("Matheus", "matheus@azevedo.com")
-me.name = "Matheus Azevedo"
 
-print(me.name)
+me = User("Keith", "keith@example.com")
 
-
-
-print(f" Active {me.is_active()}  Active users: {User.active_users}")
+print(f"Active: {me.is_active()} Active Users: {User.active_users}")
 me.activate()
-me.activate()
-print(f" Active {me.is_active()} Active users: {User.active_users}")
+print(f"Active: {me.is_active()} Active Users: {User.active_users}")
 me.deactivate()
-me.deactivate()
-print(f" Active {me.is_active()} Active users: {User.active_users}")
+print(f"Active: {me.is_active()} Active Users: {User.active_users}")
 
-
-print(f"Active users off of 'me': {me.active_users}, Class Level: {User.active_users}" )
+print(f"Active users off of `me`: {me.active_users}, Class level: {User.active_users}")
+me.active_users = "Just me"
+print(f"Active users off of `me`: {me.active_users}, Class level: {User.active_users}")
